dfbfe9af9d90a5a9edb98ee0edfd347fc515817b
Prateek <teeksingh@gmail.com>
2023-05-11 19:44:03 -0500

    Stages 1,2,3

---
+++ planning.md
@@ -0,0 +1,14 @@
+Stage 1
+[x]get the form and attach an event listener to the form
+[x]get the task from the input
+[x]display the data
+
+Stage 2
+[x] add delete button to each item on the list
+
+Stage 3
+[] select a priority level for task before submitting
+[] change color of task according to the priority level
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners();
 });
+
+function addingEventListener() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+  console.log(e)
+  const task = e.target[0].value
+  
+  displayTask(task)
+}
+
+function displayTask(task) {
+  const taskUl = document.getElementById("tasks")
+  const taskLi = document.createElement("li")
+  const deleteButton = document.createElement("button")
+
+  deleteButton.textContent = 'x'
+  deleteButton.addEventListener("click", deleteTask)
+  taskLi.textContent = task
+  taskLi.appendChild(deleteButton)
+  taskUl.appendChild(taskLi)
+
+}
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
